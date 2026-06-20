a5bce78736b2ca2955be5c3e4792dae7ca38e305
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-06-21 01:26:02 +0800

    chore: comment out api_token in provider configuration

--- providers.tf
+++ providers.tf
@@ -1,5 +1,7 @@
 
 
 provider "cloudflare" {
-  api_token = var.cloudflare_api_token
+  # api_token = var.cloudflare_api_token
+  email     = var.cloudflare_api_email != null ? var.cloudflare_api_email : null
+  api_key   = var.cloudflare_api_key != null ? var.cloudflare_api_key : null
 }
